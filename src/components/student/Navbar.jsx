731541efc9c64b82d4d7f0a9d66ebbe64abd2292
twahanur <thohanur143@gmail.com>
2025-08-21 12:32:05 +0600

    nav edit

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -86,6 +86,12 @@ const Navbar = () => {
 
       {/* Desktop View */}
       <div className="hidden md:flex items-center gap-5 text-gray-500">
+        <Link to="/course-list" className="hover:text-blue-600 border-b-2 border-transparent hover:border-blue-600 transition-all">
+          Course List
+        </Link>
+        <Link to="/about-us" className="hover:text-blue-600 border-b-2 border-transparent hover:border-blue-600 transition-all">
+          About Us
+        </Link>
         {user && (
           <div className="flex items-center gap-5">
             <button 
